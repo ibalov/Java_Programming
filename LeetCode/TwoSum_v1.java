b5ed5048caaf1e6068ade7c9d28322d6255614d3
Maik Ibalov <ibalov1510@gmail.com>
2024-11-17 22:59:18 +0100

    Update TwoSum_v1.java

--- LeetCode/TwoSum_v1.java
+++ LeetCode/TwoSum_v1.java
@@ -12,7 +12,7 @@ public class main {
         
         System.out.println("Choose method");
         System.out.println("1 - BruteForce (bucle for)");
-        System.out.println("2 - Two pass Hash Map");
+        System.out.println("2 - Two pass Has Map");
         System.out.println("3 - One pass HashTable");
         option = sc.nextInt();
         
