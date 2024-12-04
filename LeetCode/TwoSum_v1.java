8f2a69df53ba1c50a5621c4c5dbc8b8a7b7a4149
Maik Ibalov <ibalov1510@gmail.com>
2024-12-04 22:50:47 +0100

    Update TwoSum_v1.java

--- LeetCode/TwoSum_v1.java
+++ LeetCode/TwoSum_v1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scan ner(System.in);
+        Scanner sc = new Scanner(System.in);
         int option;
         
         System.out.println("Choose method");
